2fefea56e76936d8001a48c40de19fc7dd36e6e4
Andrea Barchi <89159367+AndreaBarchi@users.noreply.github.com>
2023-05-22 18:52:11 +0200

    First commit

---
+++ src/main.ts
@@ -0,0 +1,57 @@
+const mainCanvasId = "main-canvas";
+const mainCanvas = document.getElementById(mainCanvasId) as HTMLCanvasElement;
+const canvasHeight = 800;
+const canvasWidth = 800;
+const tileHeight = 32;
+const tileWidth = 32;
+
+interface Tile {
+    col: number;
+    row: number;
+    value: number;
+}
+
+if (mainCanvas == null) {
+    console.log("No element with id: ", mainCanvasId);
+}
+
+mainCanvas.height = canvasHeight+1;
+mainCanvas.width = canvasWidth+1;
+
+const context = mainCanvas.getContext("2d");
+console.log(context);
+
+let canvasArea = (mainCanvas.height * mainCanvas.width);
+let tileArea = (tileHeight * tileWidth);
+const numberOfTiles = canvasArea / tileArea;
+console.log("Number of tiles: ", numberOfTiles);
+console.log("Tile area: ", tileArea);
+
+let tiles = new Array<Tile>;
+// tiles.fill({value: 0});
+// console.log(tiles);
+
+for(let col = 1; col < mainCanvas.width; col += tileWidth) {
+    for(let row = 1; row < mainCanvas.height; row += tileHeight) {
+        let tile: Tile = {
+            col: col/tileWidth,
+            row: row/tileHeight,
+            value: 0
+        }
+        tiles.push(tile);
+        context?.fillRect(col, row, tileWidth-1, tileHeight-1);
+    }
+}
+console.log(tiles);
+
+mainCanvas.addEventListener("click", (event)=> {
+    let col = Math.floor(Math.abs(event.offsetX / tileWidth));
+    let row = Math.floor(Math.abs(event.offsetY / tileHeight));
+    // console.log("Clicked at: ", event.offsetX + " : " + event.offsetY);
+    console.log("Clicked tile at col: " + col + " and row: " + row);
+});
+
+function fillTile(col:number, row:number): void {
+    context.fillStyle = "green";
+    context?.fillRect(col, row, tileWidth-1, tileHeight-1);
+}
